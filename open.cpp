360e5dde9bc0680a18a3a8bbbcd75c625b1e5a51
Jollyproxi <davide.giolitti@gmail.com>
2023-09-24 02:40:26 +0200

    operative files

---
+++ open.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	
+    system("git pull");	
+	system("explorer http://localhost:8080"); 
+	system("python -m http.server 8080");
+	
+return 0;
+
+    
+}
+
